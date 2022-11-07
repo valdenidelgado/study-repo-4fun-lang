a3469c922b9ffec40897ddf0bb1e7f729a573821
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-11-06 23:24:22 -0300

    reoganize this repo, all files moved to spring projects

--- src/main/java/com/project/demoproject/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.demoproject.repositories;
-
-import com.project.demoproject.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
