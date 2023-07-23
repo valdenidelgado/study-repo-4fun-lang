6ea8eab801b8aa4d18f8e35032342a92517610b7
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-22 22:07:51 -0300

    fix folders

--- Golang/beecrowd-challenges/bee1000-1020/bee-1017.go
+++ Golang/beecrowd-challenges/bee1000-1020/bee-1017.go
@@ -4,18 +4,11 @@ import "fmt"
 
 func main() {
 	lt := 12
-	var tg float32
-	fmt.Scanln(&tg)
+	var tg float32; fmt.Scanln(&tg)
 
-  var vm float32
-
-  fmt.Scanln(&vm)
-
-
-
-
-
+	var vm float32; fmt.Scanln(&vm)
+	
 	calc := tg * vm / float32(lt)
-
+	
 	fmt.Printf("%.3f\n", calc)
 }
