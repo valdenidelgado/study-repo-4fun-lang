a0c8375e8de200b5b2c726c93108f344bfd1d65e
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-24 20:04:52 -0300

    finish refactor hello

--- Golang/lgwt/hello/hello.go
+++ Golang/lgwt/hello/hello.go
@@ -1,16 +1,30 @@
 package main
 
-import "fmt"
+const (
+	french  = "French"
+	spanish = "Spanish"
 
-const englishHelloPrefix = "Hello, "
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func main() {
-    fmt.Println(Hello("World"))
-}
-
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
